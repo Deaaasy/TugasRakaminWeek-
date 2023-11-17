909467345b5eb5a790db4d0f6c90461ebebd2cae
Deaaasy <zdheasyafila@gmail.com>
2023-11-17 18:32:14 +0700

    week8

---
+++ Tutorial/server/db/Zahra.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (201, 'Inaki', 'Godoy', '2023-10-7 18:09:57.62');
+
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (202, 'Mackenyu', 'Arata', '2023-10-7 18:09:57.62');
+
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (203, 'Emily', 'Rudd', '2023-10-7 18:09:57.62');
+
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (204, 'Jacob', 'Smith', '2023-10-7 18:09:57.62');
+
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (205, '  Taz', 'skylar', '2023-10-7 18:09:57.62');
